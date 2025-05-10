3dfa2fca9d1eb15d15def00bce7c9587a6232cb3
thescottytbe <scottybe@tbdstud.io>
2025-05-09 21:59:12 -0500

    chore: repo cleanup, flatten dirs, update .gitignore, remove .backup, and modernize config references

--- backup/packages/core/src/services/pdf/templates/index.ts
+++
@@ -1,4 +0,0 @@
-export { BasicTemplate } from './basic-template.js';
-export { MinimalTemplate } from './minimal-template.js';
-export { AcademicTemplate } from './academic-template.js';
-export { FederalTemplate } from './federal-template.js';
